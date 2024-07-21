f7cd770f788c3190fc9f250369455edfa69008cc
23Bhupesh <bhupeshsahu2312@gmail.com>
2024-07-21 23:58:52 +0530

    checking the footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           We&apos;re Student&apos;s welface club.
         </h1> */}
 
-        <div className="flex gap-2">
+        {/* <div className="flex gap-2">
           <Link
             href="https://www.instagram.com/coders.dyp/"
             target="_blank"
@@ -86,7 +86,7 @@ export default function Home() {
               className="h-12 w-12 hover:cursor-pointer"
             />
           </Link>
-        </div>
+        </div> */}
       </div>
 
       {/* Scrolling Divisions */}
@@ -116,12 +116,12 @@ export default function Home() {
 
       {/* About us */}
       <div className="h-screen w-full text-white flex flex-col p-10">
-        <p className="font-inter font-normal text-xl h-6 w-28 mx-5">ABOUT US</p>
+        {/* <p className="font-inter font-normal text-xl h-6 w-28 mx-5">ABOUT US</p> */}
 
         {/* Parent div */}
         <div className="flex">
           {/* Text */}
-          <div className="w-1/2 flex flex-col gap-4 p-5">
+          {/* <div className="w-1/2 flex flex-col gap-4 p-5">
             <div className="h-24 w-[90%] font-inter font-bold text-2xl">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.
@@ -131,10 +131,10 @@ export default function Home() {
               the 1500s, when an unknown printer took a galley of type and
               scrambled it to make a type specimen book.
             </div>
-          </div>
+          </div> */}
 
           {/* photo */}
-          <div className="w-1/2 p-5">
+          {/* <div className="w-1/2 p-5">
             <Image
               height={400}
               width={600}
@@ -142,25 +142,25 @@ export default function Home() {
               alt=""
               className=""
             />
-          </div>
+          </div> */}
         </div>
         {/* Button */}
-        <div className="mx-5">
+        {/* <div className="mx-5">
           <button className="border-2 w-44 h-14 ">MEET THE TEAM</button>
-        </div>
+        </div> */}
       </div>
 
       {/* recent Events */}
-      <div>
+      {/* <div>
         <h1 className="mx-auto my-20 h-9 w-auto font-roboto font-bold text-4xl text-center tracking-wider">
           RECENT EVENTS
         </h1>
         <RecentEventLeft />
         <RecentEventRight />
-      </div>
+      </div> */}
 
       {/* Gallery */}
-      <div>
+      {/* <div>
         <h1 className="mx-auto my-20 h-9 w-auto font-roboto font-bold text-4xl text-center tracking-wider">
           GALLERY
         </h1>
@@ -180,7 +180,7 @@ export default function Home() {
             animation="animate-scrollTopBottom"
           />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
