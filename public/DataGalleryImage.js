fe6bb2cb2924e142b4509e82529afd2e4efc016a
23Bhupesh <bhupeshsahu2312@gmail.com>
2024-08-10 04:14:09 +0530

    gallery updated

--- public/DataGalleryImage.js
+++ public/DataGalleryImage.js
@@ -1,24 +1,21 @@
 export const imagesColumn1 = [
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_1_1.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_1_2.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_1_3.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_1_4.png", alt: "", marginClass: "mt-10" },
 ];
 
 export const imagesColumn2 = [
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_2_1.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_2_2.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_2_3.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_2_4.png", alt: "", marginClass: "mt-10" },
 ];
 
 export const imagesColumn3 = [
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
-  { height: 250, width: 350, src: "/images/img_gallery.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_3_1.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_3_2.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_3_3.png", alt: "", marginClass: "mt-10" },
+  { height: 250, width: 350, src: "/images/img_gall_col_3_4.png", alt: "", marginClass: "mt-10" },
 
 ];
