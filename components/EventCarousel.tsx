afe7c3bf7ce9b1a731ff14c4026441b3accfe1f5
23Bhupesh <bhupeshsahu2312@gmail.com>
2024-10-07 10:51:46 +0530

    Fixed:images-problem

--- components/EventCarousel.tsx
+++ components/EventCarousel.tsx
@@ -4,11 +4,11 @@ import DataCarouselImg from "../public/DataCarouselImg.js";
 
 const EventCarousel = () => {
   return (
-    <div className="w-[100%] h-auto bg-[#ffffff] flex">
-      <Marquee gradient={false} speed={200} pauseOnHover={true} className="flex items-center h-[100%]">
+    <div className="w-[100%] bg-[#ffffff] flex">
+      <Marquee gradient={false} speed={100} pauseOnHover={true} className="flex items-center h-[100%]">
         <ul className=" flex items-center">
         {DataCarouselImg.map((e: any, index: number) => (
-          <Image key={index} height={e.height} width={e.width} src={e.src} alt={e.alt} className=""/>
+          <Image key={index} height={e.height} width={e.width} src={e.src} alt={e.alt} className="border-x-white border-2 w-auto lg:h-52 md:h-40 h-28"/>
         ))}
         </ul>
       </Marquee>
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -11,20 +11,6 @@ const index = () => {
       <Navbar />
       <div className="text-white pt-20 w-full">
         <div className="h-auto w-full">
-          {/* <div className="h-auto w-[90%] mx-auto relative flex flex-col items-center">
-          <Image
-            height={560}
-            width={1300}
-            src="/images/img_event_head.svg"
-            alt="Image"
-          />
-          <div className="absolute bottom-5 h-auto w-auto font-lemonmilk font-bold">
-            <p className="text-right text-xl tracking-wider">
-              A timeline worth watching
-            </p>
-            <h1 className="text-7xl">Our Coders Chronicles</h1>
-          </div>
-        </div> */}
           <div className="w-full h-auto">
             <h1 className="mx-auto my-10 mb-10 h-9 w-auto font-lemonmilk font-bold lg:text-5xl md:text-4xl text-2xl text-center tracking-wider">
               EVENTS
@@ -43,7 +29,7 @@ const index = () => {
           </div>
         </div>
         {/* More events */}
-        <div>
+        <div className="">
           <EventCarousel />
         </div>
         {/* statement */}
